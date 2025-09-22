dc51f5557f3eddfac369b5d286528703014ce0b0
Alon Abudraham <alonabudraham2@gmail.com>
2025-09-22 09:12:50 +0300

    counter value

--- secure-hardware/TRNG/combined_trng/src/rng.c
+++ secure-hardware/TRNG/combined_trng/src/rng.c
@@ -26,6 +26,8 @@ uint8_t rng_get_byte_counter(void)
     return counter_value;
 }
 
+
+
 uint8_t rng_get_byte_vcc_temp(void)
 {
     // Sample bytes based on VCC and TEMP ADC readings
@@ -63,6 +65,8 @@ uint8_t rng_get_crc_byte(void)
     return CRC.CHECKSUM0 ^ CRC.CHECKSUM1 ^ CRC.CHECKSUM2 ^ CRC.CHECKSUM3;
 }
 
+
+
 uint8_t get_random_bytes(uint8_t cmd, uint8_t scmd, uint8_t dlen, uint8_t *data)
 {
     // A simple serial command to get random bytes (based on scmd the type of the RNG)
@@ -89,5 +93,32 @@ uint8_t get_random_bytes(uint8_t cmd, uint8_t scmd, uint8_t dlen, uint8_t *data)
     }
 
     simpleserial_put('r', N, out);
+    return 0;
+}
+
+
+uint16_t rng_get_counter(void)
+{
+    // Wait for RTC overflow event
+    while (!(RTC.INTFLAGS & RTC_OVFIF_bm))
+    {
+        ;
+    }
+
+    // Clear the RTC overflow flag
+    RTC.INTFLAGS = RTC_OVFIF_bm;
+
+    // Read the current TCC0 counter value and reset it
+    uint16_t counter_value = TCC0.CNT;
+    TCC0.CNT = 0;
+
+    // Return counter_value as the random byte
+    return counter_value;
+}
+
+uint8_t get_counter_value(uint8_t cmd, uint8_t scmd, uint8_t dlen, uint8_t *data) {
+    uint16_t v = rng_get_counter();
+    uint8_t out[2] = { (uint8_t)(v >> 8),(uint8_t)(v & 0xFF) }; // LSB first
+    simpleserial_put('r', 2, out);
     return 0;
 }
